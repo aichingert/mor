4c15e5f90464945f40c212fc44bfd8068f7c9ae0
aichingert <aichinger.tobias13@gmail.com>
2024-05-09 13:44:23 +0200

    feat: able to compile calculations

--- src/main.rs
+++ src/main.rs
@@ -226,10 +226,12 @@ pub struct Compiler {
     bss:  Vec<String>,
 }
 
+fn ts(s:&str) -> String { s.to_string() }
+
 impl<'ex> Compiler {
     pub fn new() -> Self {
         let conv = |v: Vec<&str>| {
-            v.into_iter().map(|s| s.to_string()).collect::<Vec<_>>()
+            v.into_iter().map(|s| ts(s)).collect::<Vec<_>>()
         };
 
         let data = conv(vec!["segment    .data", "    result dd 0", ""]);
@@ -240,7 +242,6 @@ impl<'ex> Compiler {
     }
 
     fn compile_expr(&mut self, expr: Expr<'ex>) {
-        // println!("{expr:?}");
         match expr {
             Expr::BiOp(ex) => {
                 let [a, b] = ex.children;
@@ -248,29 +249,20 @@ impl<'ex> Compiler {
                 self.compile_expr(a);
                 self.compile_expr(b);
 
-                match ex.kind {
-                    BiOpKind::Add => {
-                        self.text.push("    pop eax".to_string());
-                        self.text.push("    pop ebx".to_string());
-                        self.text.push("    add eax, ebx".to_string());
-                        self.text.push("    push eax".to_string());
-                    }
-                    BiOpKind::Sub => {
-                        self.text.push("    pop eax".to_string());
-                        self.text.push("    pop ebx".to_string());
-                        self.text.push("    sub eax, ebx".to_string());
-                        self.text.push("    test eax, eax".to_string()); // is negative?
-                        self.text.push("    js negative_result".to_string());
-                        self.text.push("    push eax".to_string());
+                self.text.push(ts("    pop ebx"));
+                self.text.push(ts("    pop eax"));
 
+                match ex.kind {
+                    BiOpKind::Add => self.text.push(ts("    add eax, ebx")),
+                    BiOpKind::Sub => self.text.push(ts("    sub eax, ebx")),
+                    BiOpKind::Mul => self.text.push(ts("    imul bx")),
+                    BiOpKind::Div => {
+                        self.text.push(ts("    cdq"));
+                        self.text.push(ts("    idiv ebx"));
                     }
-                    BiOpKind::Mul => (),
-                    BiOpKind::Div => (),
                 }
 
-                // 1 + 2 * 3
-                //
-                //
+                self.text.push(ts("    push eax"));
             }
             Expr::UnOp(ex) => (), // TODO: implement unary ops
             Expr::Number(num) => {
@@ -281,64 +273,59 @@ impl<'ex> Compiler {
 
     pub fn compile(&mut self, expr: Expr<'ex>) {
         self.compile_expr(expr);
-        self.text.push("    pop eax".to_string());
-        self.text.push("    mov [result], eax".to_string());
-        self.text.push("    push ecx".to_string());
+        self.text.push(ts("    pop eax"));
+        self.text.push(ts("    mov [result], eax"));
+        self.text.push(ts("    push ecx"));
 
-        self.text.push("".to_string());
-        self.text.push("    mov ecx, [result]".to_string());
-        self.text.push("    push ecx".to_string());
-        self.text.push("".to_string());
-        self.text.push("    lea esi, [buffer]".to_string());
-        self.text.push("    mov eax, [result]".to_string());
-        self.text.push("    call int_to_string".to_string());
-        self.text.push("".to_string());
-        self.text.push("    mov ecx, eax".to_string());
-        self.text.push("    xor edx, edx".to_string());
-        self.text.push("getlen:".to_string());
-        self.text.push("    cmp byte [ecx + edx], 10".to_string());
-        self.text.push("    jz gotlen".to_string());
-        self.text.push("    inc edx".to_string());
-        self.text.push("    jmp getlen".to_string());
-        self.text.push("gotlen:".to_string());
-        self.text.push("    inc edx".to_string());
-        self.text.push("".to_string());
-        self.text.push("    mov eax, 4".to_string());
-        self.text.push("    mov ebx, 1".to_string());
-        self.text.push("    int 0x80".to_string());
-        self.text.push("".to_string());
-        self.text.push("    pop ecx".to_string());
-        self.text.push("".to_string());
-        self.text.push("    mov eax, 1".to_string());
-        self.text.push("    mov ebx, 0".to_string());
-        self.text.push("    int 0x80".to_string());
-        self.text.push("".to_string());
+        self.text.push(ts(""));
+        self.text.push(ts("    mov ecx, [result]"));
+        self.text.push(ts("    push ecx"));
+        self.text.push(ts(""));
+        self.text.push(ts("    lea esi, [buffer]"));
+        self.text.push(ts("    mov eax, [result]"));
+        self.text.push(ts("    call int_to_string"));
+        self.text.push(ts(""));
+        self.text.push(ts("    mov ecx, eax"));
+        self.text.push(ts("    xor edx, edx"));
+        self.text.push(ts("getlen:"));
+        self.text.push(ts("    cmp byte [ecx + edx], 10"));
+        self.text.push(ts("    jz gotlen"));
+        self.text.push(ts("    inc edx"));
+        self.text.push(ts("    jmp getlen"));
+        self.text.push(ts("gotlen:"));
+        self.text.push(ts("    inc edx"));
+        self.text.push(ts(""));
+        self.text.push(ts("    mov eax, 4"));
+        self.text.push(ts("    mov ebx, 1"));
+        self.text.push(ts("    int 0x80"));
+        self.text.push(ts(""));
+        self.text.push(ts("    pop ecx"));
+        self.text.push(ts(""));
+        self.text.push(ts("    mov eax, 1"));
+        self.text.push(ts("    mov ebx, 0"));
+        self.text.push(ts("    int 0x80"));
+        self.text.push(ts(""));
 
-        self.text.push("    mov eax, 1".to_string());
-        self.text.push("    xor ebx, ebx".to_string());
-        self.text.push("    int 0x80".to_string());
-        self.text.push("".to_string());
+        self.text.push(ts("    mov eax, 1"));
+        self.text.push(ts("    xor ebx, ebx"));
+        self.text.push(ts("    int 0x80"));
+        self.text.push(ts(""));
 
-        self.text.push("int_to_string:".to_string());
-        self.text.push("    add esi, 9".to_string());
-        self.text.push("    mov byte [esi], 10".to_string());
-        self.text.push("    mov ebx, 10".to_string());
-        self.text.push(".next_digit:".to_string());
-        self.text.push("    xor	edx, edx".to_string());
-        self.text.push("    div	ebx".to_string());
-        self.text.push("    add	dl, '0'".to_string());
-        self.text.push("    dec	esi".to_string());
-        self.text.push("    mov	[esi],dl".to_string());
-        self.text.push("    test	eax, eax".to_string());
-        self.text.push("    jnz	.next_digit".to_string());
-        self.text.push("    mov	eax, esi".to_string());
-        self.text.push("    ret".to_string());
+        self.text.push(ts("int_to_string:"));
+        self.text.push(ts("    add esi, 9"));
+        self.text.push(ts("    mov byte [esi], 10"));
+        self.text.push(ts("    mov ebx, 10"));
+        self.text.push(ts(".next_digit:"));
+        self.text.push(ts("    xor	edx, edx"));
+        self.text.push(ts("    div	ebx"));
+        self.text.push(ts("    add	dl, '0'"));
+        self.text.push(ts("    dec	esi"));
+        self.text.push(ts("    mov	[esi],dl"));
+        self.text.push(ts("    test	eax, eax"));
+        self.text.push(ts("    jnz	.next_digit"));
+        self.text.push(ts("    mov	eax, esi"));
+        self.text.push(ts("    ret"));
 
-        self.text.push("negate_int:".to_string();
-        self.text.push("    pop eax".to_string();
-        self.text.push("    neg eax".to_string();
-        self.text.push("    ret".to_string());
- 
         let mut file = std::fs::File::create("prog.asm").unwrap();
         file.write_all(&self.data.join("\n").bytes().collect::<Vec<_>>()).unwrap();
         file.write_all(&self.bss.join("\n").bytes().collect::<Vec<_>>()).unwrap();
