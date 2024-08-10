a67c92e1abf6c3866c7051b14295285030648b85
aichingert <aichinger.tobias13@gmail.com>
2024-08-10 23:28:51 +0200

    feat(lex): started working on basic lexer

--- morl/src/main.zig
+++ morl/src/main.zig
@@ -1,7 +1,26 @@
 const std = @import("std");
+const lex = @import("lexer.zig");
 
 pub fn main() !void {
-    std.debug.print("morl", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    // Parse args into string array (error union needs 'try')
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    for (args[1..]) |arg| {
+        var file = try std.fs.cwd().openFile(arg, .{});
+        defer file.close();
+
+        const buf = try file.readToEndAlloc(allocator, 4 * 1024 * 1024);
+        defer allocator.free(buf);
+
+        var lexer = lex.Lexer.init(buf);
+        std.debug.print("{?}\n", .{lexer.next()});
+        std.debug.print("{s}", .{buf});
+    }
 }
 
 test "simple test" {
